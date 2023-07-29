e9982236ddcebd1347ec1a3ed4cba7b6aece2635
ekazukii <baptisteloison8400@gmail.com>
2023-07-29 13:15:01 +0200

    feat: data cleaning

--- app/components/ResultItem.tsx
+++ app/components/ResultItem.tsx
@@ -1,26 +1,54 @@
 import { Card, CardBody, CardHeader, Img, Link, Tag, Text, Tooltip } from '@chakra-ui/react';
 import { SearchItem } from './data';
 import GitHubIcon from './GitHubIcon';
+import { useEffect, useState } from 'react';
+
+const httpTagRegex = /<[^>]*>?/gm;
+const markdownLinkRegex = /\]\(https.*\)/gm;
+const markdownImageRegex = /\[!\[.*\]\[.*]/gm;
+const startingPattern = /^ *[-—\|]* */gm;
+const emptyLinePattern = /^[-—\| $./]*$/gm;
+const removeTrailingPattern = /[-—\| ]*$/gm;
 
 export default function ResultItem({ item, onClick }: { item: SearchItem; onClick: (tag: string) => void }) {
+  const [cleanItem, setCleanItem] = useState<SearchItem | null>(null);
+
+  useEffect(() => {
+    let origin_description = item.origin_description;
+    if (origin_description) {
+      origin_description = origin_description
+        .replace(httpTagRegex, '')
+        .replace(markdownLinkRegex, '')
+        .replace(markdownImageRegex, '')
+        .replace(startingPattern, '')
+        .replace(emptyLinePattern, '')
+        .replace(removeTrailingPattern, '');
+    }
+    setCleanItem({ ...item, origin_description });
+  }, [item]);
+
+  if (!cleanItem) {
+    return <></>;
+  }
+
   return (
     <Card size="sm" width="76vw">
       <CardHeader>
         <Text pt={0}>
-          <Tooltip label={item.description}>
-            <Link mx={2} href={item.URL} isExternal color="teal.500">
-              {item.displayName ? item.displayName : 'No title found'} {item.isGithub && <GitHubIcon />}
+          <Tooltip label={cleanItem.description}>
+            <Link mx={2} href={cleanItem.URL} isExternal color="teal.500">
+              {cleanItem.displayName ? cleanItem.displayName : 'No title found'} {cleanItem.isGithub && <GitHubIcon />}
             </Link>
           </Tooltip>
-          - {item.origin_description}
+          - {cleanItem.origin_description}
           <Tag
             ml={2}
             color="teal"
             cursor="pointer"
             _hover={{ textDecor: 'underline' }}
-            onClick={() => onClick(item.repositoryname)}
+            onClick={() => onClick(cleanItem.repositoryname)}
           >
-            {item.repositoryname}
+            {cleanItem.repositoryname}
           </Tag>
         </Text>
       </CardHeader>
